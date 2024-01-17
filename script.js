696c2a7b03465186aa9615edd0c7b20dd38ab71c
Dean Harris <dean@harris.tc>
2024-01-17 21:42:05 +1000

    Add initial text and convert it to a dialog when populating params into body

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const capitalise = (word) => word.charAt(0).toUpperCase() + word.slice(1);
 const createCopyButton = (content) => {
   const button = document.createElement('button');
   button.setAttribute('type', 'button');
-  button.innerHTML = `<svg><use xlink:href="#copy-icon" /></svg>`;
+  button.innerHTML = `Copy`;
   button.addEventListener('click', () =>
     window.navigator.clipboard.writeText(content),
   );
@@ -12,21 +12,51 @@ const createCopyButton = (content) => {
 
 const createParamBlock = ([name, value]) => {
   const block = document.createElement('article');
-  // header
   block.innerHTML += `<header><h2>${capitalise(name)}</h2></header>`;
-  // content
   block.innerHTML += `<div class="param-value">${value}</div>`;
-  // copy button
   block.appendChild(createCopyButton(value));
 
   return block;
 };
 
-if (window.location.search) {
-  const params = new URLSearchParams(window.location.search);
-  const outputEl = document.getElementById('output');
-  outputEl.innerText = '';
-  for (const param of params.entries()) {
-    outputEl.appendChild(createParamBlock(param));
+const copyContentToDialog = () => {
+  const article = document.querySelector('#output article');
+  const closeButton = document.createElement('button');
+  closeButton.setAttribute('type', 'button');
+  closeButton.innerHTML = `Close`;
+  closeButton.addEventListener('click', () => dialog.close());
+
+  article.appendChild(closeButton);
+
+  const dialog = document.createElement('dialog');
+  dialog.appendChild(article);
+
+  document.addEventListener('keypress', (e) => {
+    if (e.key === '?') {
+      dialog.showModal();
+    }
+    if (e.key === 'Escape') {
+      dialog.close();
+    }
+  });
+
+  dialog.addEventListener('click', (e) => {
+    if (e.target === dialog) {
+      dialog.close();
+    }
+  });
+
+  document.querySelector('body').insertAdjacentElement('beforeend', dialog);
+};
+
+(() => {
+  const output = document.getElementById('output');
+  if (window.location.search) {
+    copyContentToDialog();
+    const params = new URLSearchParams(window.location.search);
+    output.innerText = '';
+    for (const param of params.entries()) {
+      output.appendChild(createParamBlock(param));
+    }
   }
-}
+})();
